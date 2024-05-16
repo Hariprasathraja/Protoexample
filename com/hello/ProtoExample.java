43e914411037c5ce8febe1165f5a3c32c8cce955
Hariprasathraja <hariprasathraja6023@gmail.com>
2024-05-16 18:49:01 +0530

    log: Using Hashmap to store the details of a customer.

--- com/hello/ProtoExample.java
+++ com/hello/ProtoExample.java
@@ -1,12 +1,27 @@
 package com.hello;
 import com.hello.Hello.hellorequest;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProtoExample {
     public static void main(String[] args) {
-        hellorequest.Builder request= hellorequest.newBuilder();
+        Map<Integer, hellorequest> accountDetails = new HashMap<>();
+        hellorequest account1 = hellorequest.newBuilder()
+                                            .setName("John")
+                                            .build();
+        accountDetails.put(123456, account1);
 
-        // Print the messa
-        request.setName("hari");
-        System.out.println(request.getName());
+        hellorequest account2 = hellorequest.newBuilder()
+                                            .setName("Alice")
+                                            .build();
+        accountDetails.put(987654, account2);
+        System.out.println("Bank Account Details:");
+        for (Map.Entry<Integer, hellorequest> entry : accountDetails.entrySet()) {
+            int accountNumber = entry.getKey();
+            hellorequest account = entry.getValue();
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Name: " + account.getName());
+            System.out.println();
+        }
     }
 }
