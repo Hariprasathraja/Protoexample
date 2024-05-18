651676a64786dd29199b6b226e971348921f8e70
Hariprasathraja <hariprasathraja6023@gmail.com>
2024-05-18 16:06:26 +0530

    Created a new file and stored the data in there and created two method for load and save the file.

--- com/hello/ProtoExample.java
+++ com/hello/ProtoExample.java
@@ -1,11 +1,13 @@
 package com.hello;
 
+import java.io.*;
 import com.hello.Hello.hellorequest;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ProtoExample {
+    private static final String file_name="/home/hpr/Protoexample/com/hello/data.text";
     public static void main(String[] args) {
         Map<Integer, hellorequest> accountDetails = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
@@ -27,9 +29,11 @@ public class ProtoExample {
                                                .build();
             accountDetails.put(accountNumber, account);
         }
+        save(accountDetails);
 
-        System.out.println("Bank Account Details:");
-        for (Map.Entry<Integer, hellorequest> entry : accountDetails.entrySet()) {
+        System.out.println("Bank Account Details loaded from file:\n");
+        Map<Integer,hellorequest> loadedAccountDetails= load();
+        for (Map.Entry<Integer, hellorequest> entry : loadedAccountDetails.entrySet()) {
             int accountNumber = entry.getKey();
             hellorequest account = entry.getValue();
             System.out.println("Account Number: " + accountNumber);
@@ -38,5 +42,23 @@ public class ProtoExample {
         }
 
         scanner.close();
+    }
+    private static void save(Map<Integer,hellorequest> accountDetails){
+        try(ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(file_name))){
+            oos.writeObject(accountDetails);
+            System.out.println("Account details saved");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    @SuppressWarnings("unchecked")
+    private static Map<Integer,hellorequest> load(){
+        Map<Integer,hellorequest> Details=new HashMap<>();
+        try(ObjectInputStream ois=new ObjectInputStream(new FileInputStream(file_name))){
+            Details=(Map<Integer,hellorequest>)ois.readObject();
+        } catch(IOException| ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return Details;
     }
 }
