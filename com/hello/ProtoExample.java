162a41fbcb91cf41213bfcb43f02a58f1a9e094c
Hariprasathraja <hariprasathraja6023@gmail.com>
2024-05-18 13:52:03 +0530

    Changed to user input

--- com/hello/ProtoExample.java
+++ com/hello/ProtoExample.java
@@ -1,20 +1,33 @@
 package com.hello;
+
 import com.hello.Hello.hellorequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ProtoExample {
     public static void main(String[] args) {
         Map<Integer, hellorequest> accountDetails = new HashMap<>();
-        hellorequest account1 = hellorequest.newBuilder()
-                                            .setName("Name1")
-                                            .build();
-        accountDetails.put(123456, account1);
+        Scanner scanner = new Scanner(System.in);
 
-        hellorequest account2 = hellorequest.newBuilder()
-                                            .setName("Name2")
-                                            .build();
-        accountDetails.put(987654, account2);
+        System.out.print("Enter the number of accounts: ");
+        int numAccounts = scanner.nextInt();
+        scanner.nextLine();  // Consume newline
+
+        for (int i = 0; i < numAccounts; i++) {
+            System.out.print("Enter account number: ");
+            int accountNumber = scanner.nextInt();
+            scanner.nextLine();  // Consume newline
+
+            System.out.print("Enter name: ");
+            String name = scanner.nextLine();
+
+            hellorequest account = hellorequest.newBuilder()
+                                               .setName(name)
+                                               .build();
+            accountDetails.put(accountNumber, account);
+        }
+
         System.out.println("Bank Account Details:");
         for (Map.Entry<Integer, hellorequest> entry : accountDetails.entrySet()) {
             int accountNumber = entry.getKey();
@@ -23,5 +36,7 @@ public class ProtoExample {
             System.out.println("Name: " + account.getName());
             System.out.println();
         }
+
+        scanner.close();
     }
 }
