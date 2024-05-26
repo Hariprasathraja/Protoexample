d5bc2fba770b35f0bfe9b0e58fb6175e87961ee1
Hariprasathraja <hariprasathraja6023@gmail.com>
2024-05-26 12:11:12 +0530

    Added a new method to transfer amount from one account to another and added another method to display all account details.

--- com/app/BankApp.java
+++ com/app/BankApp.java
@@ -16,7 +16,7 @@ public class BankApp {
         boolean exit=false;
         while(!exit){
             System.out.println("****Bank Application Menu****\n");
-            System.out.println("1. Create new Account\n2. View Account\n3. Update Account\n4. Delete Account\n5. Deposit Amount\n6. Withdraw Amount\n7. Exit");
+            System.out.println("1. Create new Account\n2. View Account\n3. Update Account\n4. Delete Account\n5. Deposit Amount\n6. Withdraw Amount\n7. Transfer Acccount\n8. Display All Account Details\n9. Exit");
             System.out.print("\nEnter your choice: ");
             int choice=scanner.nextInt();
 
@@ -40,6 +40,13 @@ public class BankApp {
                     withdrawAmount(scanner, accountDetails);
                     break;
                 case 7:
+                    transferAmount(scanner, accountDetails);
+                    break;
+                case 8:
+                    displayAccountDetails();
+                    break;
+                case 9:
+                    System.out.println("Thanks for using..");
                     exit=true;
                     saveAccountDetails(accountDetails);
                     break;
@@ -47,17 +54,7 @@ public class BankApp {
                     System.out.println("Invalid choice");
                     break;
             }
-        }
-        System.out.println("Bank Account Details loaded from file:\n");
-        Map<Integer,AccountDetails> loadedAccountDetails= loadAccountDetails();
-        for (Map.Entry<Integer, AccountDetails> entry : loadedAccountDetails.entrySet()) {
-            int accountNumber = entry.getKey();
-            AccountDetails account = entry.getValue();
-            System.out.println("Account Number: " + accountNumber);
-            System.out.println("Name: " + account.getName());
-            System.out.println("Balance: "+account.getBalance());
-            System.out.println();
-        }
+        }        
         scanner.close();
     }
 
@@ -167,6 +164,55 @@ public class BankApp {
             }
         }else{
             System.out.println("Account not found.");
+        }
+    }
+
+    //TRANSFER AMOUNT FROM ONE ACCOUNT TO ANOTHER
+    private static void transferAmount(Scanner scanner,Map<Integer,AccountDetails>accountDetails){
+        System.out.println("Enter your account no: ");
+        int fromAccountNum=scanner.nextInt();
+        scanner.nextLine();
+        AccountDetails fromAccount= accountDetails.get(fromAccountNum);
+
+        if(fromAccount!=null){
+            System.out.println("Enter the recipient account no: ");
+            int toAccountNum=scanner.nextInt();
+            scanner.nextLine();
+            AccountDetails toAccount=accountDetails.get(toAccountNum);
+
+            if(toAccount!=null){
+                System.out.println("Enter the amount to transfer: ");
+                float amount=scanner.nextFloat();
+
+                if(amount>0 && amount<=fromAccount.getBalance()){
+                    fromAccount=AccountDetails.newBuilder(fromAccount).setBalance(fromAccount.getBalance()-amount).build();
+                    toAccount=AccountDetails.newBuilder(toAccount).setBalance(toAccount.getBalance()+amount).build();
+                    accountDetails.put(fromAccountNum,fromAccount);
+                    accountDetails.put(toAccountNum, toAccount);
+                    saveAccountDetails(accountDetails);
+                    System.out.println("\n---Amount has been transferred successfully---");
+                }else{
+                    System.out.println("Insufficient funds or Invalid Amount");
+                }
+            }else{
+                System.out.println("Recipient account not found.");
+            }
+        }else{
+            System.out.println("Invalid account number.");
+        }
+    }
+
+    //DISPLAY ACCOUNT DETAILS
+    private static void displayAccountDetails(){
+        System.out.println("Bank Account Details loaded from file:\n");
+        Map<Integer,AccountDetails> loadedAccountDetails= loadAccountDetails();
+        for (Map.Entry<Integer, AccountDetails> entry : loadedAccountDetails.entrySet()) {
+            int accountNumber = entry.getKey();
+            AccountDetails account = entry.getValue();
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Name: " + account.getName());
+            System.out.println("Balance: "+account.getBalance());
+            System.out.println();
         }
     }
 
